16ae8d7f4c8589c2d3a70fc9ca3f75d5341978b8
Keisha <kmmatthews15@gmail.com>
2020-02-18 21:40:23 -0600

    staging the netlify build

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,14 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
-const dotenv = require('dotenv');
-if (process.env.NODE_ENV !== 'production') {
-  dotenv.config()
-}
+// const dotenv = require('dotenv');
+// if (process.env.NODE_ENV !== 'production') {
+//   dotenv.config()
+// }
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
   siteMetadata: {

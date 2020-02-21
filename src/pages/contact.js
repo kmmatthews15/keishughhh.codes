9ec450e402a1e5fa39984a73e41d435cb6ad6335
Keisha <kmmatthews15@gmail.com>
2020-02-20 20:51:50 -0600

    checking to see is this change works in netlify

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from '../components/layout'
 import '../styles/contact.css'
+import '../styles/projects.css'
 import reachOut from '../images/reach-out.png'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'

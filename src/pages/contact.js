b35e82928ae99122652ff64e51538248cd73494b
Keisha <kmmatthews15@gmail.com>
2020-02-23 18:06:57 -0600

    changed to styled components instead

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,36 +1,107 @@
 import React from 'react'
 import Layout from '../components/layout'
-import '../styles/contact.css'
-import '../styles/projects.css'
+import styled from 'styled-components';
 import reachOut from '../images/reach-out.png'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons'
 import { faLinkedin, faGithub, faInstagram } from "@fortawesome/free-brands-svg-icons"
 
-const Contact = () => {
-   return (
-      <div>
+const Container = styled.div`
+   text-align: center;
+   margin: 0!important;
+`
+
+const Socials = styled.div` 
+   margin-top: 2rem;
+   margin-left: 290px;
+   float: left;
+   height: 100%;
+   @media (max-width: 1400px) {
+      margin-left: 350px;
+   }
+   @media (max-width: 1300px) {
+      margin-left: 290px;
+   }
+   @media (max-width: 1000px) {
+      margin-left: 220px;
+   }
+`
+
+const Wrapper = styled.section`
+   margin-top: 8rem;
+   margin-left: 70px;
+   float: left;
+   height: 100%;
+   @media (max-width: 1400px) {
+      display: none;
+    }
+   overflow: hidden;
+   width: 45%;
+`
+
+const Right = styled.section`
+   padding-top: 20rem;
+   margin-right: 150px;
+   text-align: center;
+   float: right;
+   margin-top: 6%;
+   width: 35%;
+   font-size: 18px;
+   border-left: 5px solid white;
+   padding-left: 70px;
+   margin-bottom: 0px;
+   @media (max-width: 1400px) {
+     width: 100%;
+     font-size: 14px;
+     margin-right: 0px;
+     border: none;
+     margin-bottom: 50px;
+     padding-top: 10vw;
+     margin-top: 20%;
+    }
+`
+
+const Header = styled.p`
+   font-size: 20px;
+   font-weight: 600;
+`
+
+const Info = styled.p`
+   font-size: 16px;
+   width: 80%;
+   text-align: center;
+   margin: 0 auto;
+   margin-bottom: 30px;
+`
+
+
+class ContactPage extends React.Component {
+   render() {
+      return (
+         <div>
          <Layout>
-            <div className="contact-container">
-               <div className="contact-left">
+            <Container>
+               <Wrapper>
                   <img src={reachOut} className="reach-out" alt="pic"></img>
-               </div>
-               <div className="contact-right">
-                  <p className="connect-header">Let's connect!!</p>
-                  <p className="connect-info"> Feel free to stop by and have a chat at any of the social links below!
-            </p>
-                  <div>
-                     <a href="https://www.linkedin.com/in/keisha-matthews-794a55189" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} className="inner-about-contact" /></a>
-                     <a href="https://www.github.com/kmmatthews15" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} className="inner-about-contact" /></a>
-                     <a href="https://www.instagram.com/keishughhh.codes" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} className="inner-about-contact" /></a>
-                     <a href="mailto:keishughhhcodes@gmail.com?subject=Keisha Inquiry"><FontAwesomeIcon icon={faEnvelopeSquare} className="inner-about-contact" /></a>
-                  </div>
-               </div>
-            </div>
+               </Wrapper>
+            </Container>
+            <Right>
+               <Header>Let's Connect</Header>
+               <Info>Feel free to stop by anf have a chat at any of the social
+                  links below!
+               </Info>
+            </Right>
+            <Socials>
+            <a href="https://www.linkedin.com/in/keisha-matthews-794a55189" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} className="inner-about-contact" /></a>
+            <a href="https://www.github.com/kmmatthews15" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} className="inner-about-contact" /></a>
+            <a href="https://www.instagram.com/keishughhh.codes" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} className="inner-about-contact" /></a>
+            <a href="mailto:keishughhhcodes@gmail.com?subject=Keisha Inquiry"><FontAwesomeIcon icon={faEnvelopeSquare} className="inner-about-contact" /></a>
+            </Socials>
          </Layout>
       </div>
-   )
+      )
+   }
 }
 
-export default Contact
+export default ContactPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,30 +9,30 @@ import landingStyles from '../styles/landing.module.css'
 import logo from '../images/keishughhh-codes.png'
 
 const IndexPage = () => {
-   return (
-     <div>
-       <Helmet>
-       <meta charSet="utf=8" />
-            <title>Keisha Matthews</title>
-            <meta name="description" content="About Keisha - Keisha is a software developer and designer based in Nashville, TN. When Keisha's not coding, she's exploring new technologies and true crime documentaries." />
-       </Helmet>
-       <Layout>
-         <div className="index-container">
-           <h1 className={landingStyles.hello}>Hi, I'm Keisha Matthews.</h1>
-           <h2 className={landingStyles.info}>Keisha is a software developer and designer based in Nashville, TN. When Keisha's not coding, she's exploring new technologies and true crime documentaries.</h2>
-           <div className={landingStyles.surroundingDiv}>
-             <img className={landingStyles.me} src={logo} alt="Keisha"></img>
-           </div>
-         </div>
-         <div className={landingStyles.contactInfo}>
-         <a href="https://www.linkedin.com/in/keisha-matthews-794a55189" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} className="inner-about-contact" /></a>
-                  <a href="https://www.github.com/kmmatthews15" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} className="inner-about-contact" /></a>
-                  <a href="https://www.instagram.com/keishughhh.codes" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} className="inner-about-contact" /></a>
-                  <a href="mailto:keishughhhcodes@gmail.com?subject=Keisha Inquiry"><FontAwesomeIcon icon={faEnvelopeSquare} className="inner-about-contact" /></a>
-         </div>
-       </Layout>
-   </div>
-   )
- }
- 
- export default IndexPage
+  return (
+    <div>
+      <Helmet>
+        <meta charSet="utf=8" />
+        <title>Keisha Matthews</title>
+        <meta name="description" content="About Keisha - Keisha is a software developer and designer based in Nashville, TN. When Keisha's not coding, she's exploring new technologies and true crime documentaries." />
+      </Helmet>
+      <Layout>
+        <div className="index-container">
+          <h1 className={landingStyles.hello}>Hi, I'm Keisha Matthews.</h1>
+          <h2 className={landingStyles.info}>Keisha is a software developer and designer based in Nashville, TN. When Keisha's not coding, she's exploring new technologies and true crime documentaries.</h2>
+          <div className={landingStyles.surroundingDiv}>
+            <img className={landingStyles.me} src={logo} alt="Keisha"></img>
+          </div>
+        </div>
+        <div className={landingStyles.contactInfo}>
+          <a href="https://www.linkedin.com/in/keisha-matthews-794a55189" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} className="inner-about-contact" /></a>
+          <a href="https://www.github.com/kmmatthews15" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} className="inner-about-contact" /></a>
+          <a href="https://www.instagram.com/keishughhh.codes" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} className="inner-about-contact" /></a>
+          <a href="mailto:keishughhhcodes@gmail.com?subject=Keisha Inquiry"><FontAwesomeIcon icon={faEnvelopeSquare} className="inner-about-contact" /></a>
+        </div>
+      </Layout>
+    </div>
+  )
+}
+
+export default IndexPage
